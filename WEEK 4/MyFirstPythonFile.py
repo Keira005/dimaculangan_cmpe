e5b3f2b39f105c69ab923c39bf2346440b20018d
Keira005 <dimaculangankeirajustine@gmail.com>
2026-01-17 19:20:02 +0800

    Week 4

---
+++ WEEK 4/MyFirstPythonFile.py
@@ -0,0 +1,42 @@
+print("Hello World Double Quote")
+print('Hello World Single Quote')
+print('Kei Said "Hello World Single Quote!!!"')
+print("Kei Said 'Hello World Single Quote!!!'")
+
+print(input("Enter your PIN number : "))
+
+firstName = input("Enter your First Name : ")
+lastName = input("Enter your Last Name : ")
+print("Your Full name is : " + firstName + " " + lastName)
+
+myNum1 = 55
+myNum2 = 28.5
+myNum3 = 555555555555555
+myNum4 = 282828288282288282828
+
+myString = "Go! Go! Power Rangers!!!!! @#$%^&*() 55555555"
+age1 = "10"
+age2 = "9"
+sum = age1 + age2
+print(sum)
+
+myString1 = "Keira is"
+myString2 = " Pretty"
+sum = myString1 + myString2
+print(sum)
+
+print("hello kitty")
+
+a = input("Enter first number : ")
+b = input("Enter second number : ")
+c = input("Enter third number : ")
+
+mySum = int(a) + int(b) + int(c)
+
+print(mySum)
+
+myAge = 19
+myName = "Keira Justine Dimaculangan"
+
+print("I am : " + myName + " and my age is : " + str(myAge))
+print(f"I am : {myName} and my age is : {myAge}")
